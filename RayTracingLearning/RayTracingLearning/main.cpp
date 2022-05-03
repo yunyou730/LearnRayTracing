d6ea4a60c9329dd767a113090cb2385426c83a9f
ayy <502765299@qq.com>
2022-05-03 16:27:03 +0800

    +clear with bg color

--- RayTracingLearning/RayTracingLearning/main.cpp
+++ RayTracingLearning/RayTracingLearning/main.cpp
@@ -1,27 +1,55 @@
 #include <iostream>
 
-int main() {
+#include "vec3.h"
+#include "vec3_utility.h"
+#include "ray.h"
 
-	// Image
 
-	const int image_width = 256;
-	const int image_height = 256;
+color ray_color(const ray& r)
+{
+	color result;
+
+	vec3 unit_direction = unit_vector(r.direction());
+	double t = 0.5 * (unit_direction.y() + 1.0);
+
+	return (1.0 - t) * color(1.0,1.0,1.0) + t * color(0.5,0.7,1.0);
+}
+
+int main() 
+{
+	// Canvas
+	const double aspect_ratio = 16.0 / 9.0;
+	const int image_width = 400;
+	const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+
+	// Camera
+	double viewport_height = 2.0;
+	double viewport_width = aspect_ratio * viewport_height;
+	double focal_length = 1.0;
+
+	point3 origin = point3(0.0, 0.0,0.0);
+	vec3 horizontal = vec3(viewport_width,0,0);
+	vec3 vertical = vec3(0,viewport_height,0);
+	vec3 lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0,0,focal_length);
+
 
 	// Render
-
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	for (int j = image_height - 1; j >= 0; --j) 
+	{
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+		for (int i = 0; i < image_width; ++i) 
+		{
+			double u = double(i) / (image_width - 1);
+			double v = double(j) / (image_height - 1);
 
-	for (int j = image_height - 1; j >= 0; --j) {
-		for (int i = 0; i < image_width; ++i) {
-			auto r = double(i) / (image_width - 1);
-			auto g = double(j) / (image_height - 1);
-			auto b = 0.25;
+			ray r(origin,lower_left_corner + u * horizontal + v * vertical - origin);
 
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
-
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			color pixel_color = ray_color(r);//(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+			write_color(std::cout, pixel_color);
 		}
 	}
+
+	std::cerr << "\nDone.\n";
 }
---
+++ RayTracingLearning/RayTracingLearning/vec3_utility.h
@@ -0,0 +1,55 @@
+#include "vec3.h"
+
+inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+	return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3& u, const vec3& v) {
+	return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+inline vec3 operator-(const vec3& u, const vec3& v) {
+	return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) {
+	return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3& v) {
+	return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double t) {
+	return t * v;
+}
+
+inline vec3 operator/(vec3 v, double t) {
+	return (1 / t) * v;
+}
+
+inline double dot(const vec3& u, const vec3& v) {
+	return u.e[0] * v.e[0]
+		+ u.e[1] * v.e[1]
+		+ u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) {
+	return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+		u.e[2] * v.e[0] - u.e[0] * v.e[2],
+		u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(vec3 v) {
+	return v / v.length();
+}
+
+
+void write_color(std::ostream& out, color pixel_color) {
+	// Write the translated [0,255] value of each color component.
+	out << static_cast<int>(255.999 * pixel_color.x()) << ' '
+		<< static_cast<int>(255.999 * pixel_color.y()) << ' '
+		<< static_cast<int>(255.999 * pixel_color.z()) << '\n';
+}
+
+

0830a96740fdde62ddb3af321ffc46fb6cc5f5e9
ayy <502765299@qq.com>
2022-05-03 17:20:58 +0800

    +ray tracing one sphere

--- RayTracingLearning/RayTracingLearning/main.cpp
+++ RayTracingLearning/RayTracingLearning/main.cpp
@@ -5,10 +5,22 @@
 #include "ray.h"
 
 
+bool hit_sphere(const point3& center,double radius,const ray& r)
+{
+	vec3 oc = r.origin() - center;
+	double a = dot(r.direction(), r.direction());
+	double b = 2.0 * dot(oc,r.direction());
+	double c = dot(oc, oc) - radius * radius;
+	double discriminant = b * b - 4 * a * c;
+	return discriminant > 0;
+}
+
 color ray_color(const ray& r)
 {
-	color result;
-
+	if (hit_sphere(point3(0,0,-1),0.5,r))
+	{
+		return color(1, 0, 0);
+	}
 	vec3 unit_direction = unit_vector(r.direction());
 	double t = 0.5 * (unit_direction.y() + 1.0);
 
